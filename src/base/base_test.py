27f862ba4909b6ff55dbf481e6253a1a34811609
Anton Bogomolov <ashteriel@yandex.ru>
2024-11-25 01:37:59 +0200

    add constructor for test

--- src/base/base_test.py
+++ src/base/base_test.py
@@ -1,5 +1,18 @@
 from abc import ABC
 
+import pytest
+
+from src.pages import LoginPage, MainPage
+
 
 class BaseTest(ABC):
     """Класс конструктор для всех классов `*Test`"""
+
+    login_page: LoginPage
+    main_page: MainPage
+
+    @pytest.fixture(autouse=True)
+    def setup(self, driver, request):
+        request.cls.driver = driver
+        request.cls.login_page = LoginPage(driver)
+        request.cls.main_page = MainPage(driver)
