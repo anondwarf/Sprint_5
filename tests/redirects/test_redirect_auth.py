435ab11cd82b31da48a0bddba581a67a34f31bd7
Anton Bogomolov <ashteriel@yandex.ru>
2024-11-25 14:55:22 +0200

    add new test

--- tests/redirects/test_redirect_auth.py
+++ tests/redirects/test_redirect_auth.py
@@ -27,3 +27,14 @@ class TestRedirectAuth(BaseTest):
         self.main_page.click(Bpl.ACCOUNT_LINK)
         self.account_profile_page.click(locator=Bpl.CONSTRUCTOR_LINK)
         self.main_page.is_opened()
+
+    def test_account_profile_page_logo(self) -> None:
+        self.login_page.open()
+        self.login_page.enter_text(
+            locator=Lpl.LOGIN_INPUT, text_="antonbogomolov12a1234@ya.ru"
+        )
+        self.login_page.enter_text(locator=Lpl.PASSWORD_INPUT, text_="1234567")
+        self.login_page.click(locator=Lpl.LOGIN_BUTTON)
+        self.account_profile_page.open()
+        self.account_profile_page.click(Bpl.LOGO_LINK)
+        self.main_page.is_opened()
