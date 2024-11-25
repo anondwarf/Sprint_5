e7cafc392d4258f50a953cf3d06b7a12bcb23598
Anton Bogomolov <ashteriel@yandex.ru>
2024-11-25 13:21:29 +0200

    change options

--- src/config/browser_options.py
+++ src/config/browser_options.py
@@ -9,12 +9,12 @@ class BrowsersOptions:
     def chrome_options() -> ChromeOptions:
         """Настройки запуска Chrome"""
         opt = ChromeOptions()
-        opt.add_argument("--headless:new")
+        opt.add_argument("--headless")
         return opt
 
     @staticmethod
     def firefox_options() -> FirefoxOptions:
         """Настройки запуска Firefox"""
         opt = FirefoxOptions()
-        opt.add_argument("--headless:new")
+        opt.add_argument("--headless")
         return opt
