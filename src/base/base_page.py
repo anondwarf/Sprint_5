5e0e749cb140a5c547c33e5ec05e0dc75fc174d9
Anton Bogomolov <ashteriel@yandex.ru>
2024-11-25 13:21:13 +0200

    update file

--- src/base/base_page.py
+++ src/base/base_page.py
@@ -16,31 +16,41 @@ class BasePage(ABC):
         self.driver.get(self.page_url)
 
     def is_opened(self) -> None:
-        self.wait.until(ec.url_to_be(self.page_url))
+        self.wait.until(ec.url_to_be(url=self.page_url))
 
     def click(self, locator: tuple[str, str]) -> None:
-        self.wait.until(ec.element_to_be_clickable(locator)).click()
+        self.wait.until(ec.element_to_be_clickable(mark=locator)).click()
 
-    def enter_text(self, locator: tuple[str, str], text: str) -> None:
-        self.wait.until(ec.element_to_be_clickable(locator)).send_keys(text)
+    def enter_text(self, locator: tuple[str, str], text_: str) -> None:
+        self.wait.until(ec.element_to_be_clickable(mark=locator)).send_keys(
+            text_
+        )
 
     def is_entered_text(
-        self, type: str, locator: tuple[str, str], text: str, attr: str = ""
-    ) -> None:
-        match type:
+        self,
+        type_: str,
+        locator: tuple[str, str],
+        text_: str,
+        attr: str | None = None,
+    ) -> bool:
+        match type_:
             case "value":
                 self.wait.until(
-                    ec.text_to_be_present_in_element_value(locator, text)
+                    ec.text_to_be_present_in_element_value(
+                        locator=locator, text_=text_
+                    )
                 )
             case "attr":
                 self.wait.until(
                     ec.text_to_be_present_in_element_attribute(
-                        locator, attr, text
+                        locator=locator, attribute_=attr, text_=text_
                     )
                 )
             case _:
                 self.wait.until(
-                    ec.text_to_be_present_in_element(locator, text)
+                    ec.text_to_be_present_in_element(
+                        locator=locator, text_=text_
+                    )
                 )
 
     def is_displayed(self, locator: tuple[str, str]) -> bool:
