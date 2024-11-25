a5611be65a95ea280eb4bf504951cc451c7186af
Anton Bogomolov <ashteriel@yandex.ru>
2024-11-25 13:20:31 +0200

    add fixture for generate test data

--- conftest.py
+++ conftest.py
@@ -2,11 +2,22 @@ import pytest
 from selenium import webdriver
 
 from src.config import BrowsersOptions
+from src.utils import GenerationData
 
 
 @pytest.fixture(autouse=True)
 def driver(request):
-    driver = webdriver.Firefox(options=BrowsersOptions.firefox_options())
+    driver = webdriver.Chrome(options=BrowsersOptions.chrome_options())
     request.cls.driver = driver
     yield driver
     driver.quit()
+
+
+@pytest.fixture
+def generate_string(request):
+    return GenerationData.generate_string(request.param)
+
+
+@pytest.fixture
+def generate_email(request):
+    return GenerationData.generate_email(request.param)
