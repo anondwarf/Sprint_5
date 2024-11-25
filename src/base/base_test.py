9e0db54b6767187a22ea3fa351789a0565cfd44b
Anton Bogomolov <ashteriel@yandex.ru>
2024-11-25 14:08:16 +0200

    add new page

--- src/base/base_test.py
+++ src/base/base_test.py
@@ -2,7 +2,13 @@ from abc import ABC
 
 import pytest
 
-from src.pages import LoginPage, MainPage, RegistrationPage
+from src.pages import (
+    AccountProfilePage,
+    ForgotPasswordPage,
+    LoginPage,
+    MainPage,
+    RegistrationPage,
+)
 
 
 class BaseTest(ABC):
@@ -11,6 +17,8 @@ class BaseTest(ABC):
     login_page: LoginPage
     main_page: MainPage
     registration_page: RegistrationPage
+    forgot_password_page: ForgotPasswordPage
+    account_profile_page: AccountProfilePage
 
     @pytest.fixture(autouse=True)
     def setup(self, driver, request):
@@ -18,3 +26,5 @@ class BaseTest(ABC):
         request.cls.login_page = LoginPage(driver)
         request.cls.main_page = MainPage(driver)
         request.cls.registration_page = RegistrationPage(driver)
+        request.cls.forgot_password_page = ForgotPasswordPage(driver)
+        request.cls.account_profile_page = AccountProfilePage(driver)
