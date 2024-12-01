cf032d07883bd355941fb533053bceb47d82b672
Anton Bogomolov <ashteriel@yandex.ru>
2024-12-01 22:18:09 +0200

    add test constructor

--- src/locators.py
+++ src/locators.py
@@ -21,3 +21,9 @@ class Locators:
     ACCOUNT_PROFILE_EXIT_BUTTON: str = (
         '//*[@id="root"]/div/main/div/nav/ul/li[3]/button'
     )
+    MAIN_TOPPING_TAB: str = '//*[@id="root"]/div/main/section[1]/div[1]/div[3]'
+    MAIN_TOPPING_HEADER: str = '//*[@id="root"]/div/main/section[1]/div[2]/h2[3]'
+    MAIN_SAUCES_TAB: str = '//*[@id="root"]/div/main/section[1]/div[1]/div[2]'
+    MAIN_SAUCES_HEADER: str = '//*[@id="root"]/div/main/section[1]/div[2]/h2[2]'
+    MAIN_BREAD_TAB: str = '//*[@id="root"]/div/main/section[1]/div[1]/div[1]'
+    MAIN_BREAD_HEADER: str = '//*[@id="root"]/div/main/section[1]/div[2]/h2[1]'
